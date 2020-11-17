cd8bbe6b3876ba8781f4b3ac511129e0493adfef
Amri <amrikarisma@yahoo.com>
2020-11-17 22:08:59 +0700

    fix style on checkout page

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -31,33 +31,43 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
-	<?php if ( $checkout->get_checkout_fields() ) : ?>
+	<div class="row">
 
-		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+		<div class="col-12 col-sm-6">
 
-		<div class="row" id="customer_details">
-			<div class="col-12 col-sm-7">
-				<?php do_action( 'woocommerce_checkout_billing' ); ?>
-			</div>
+			<?php if ( $checkout->get_checkout_fields() ) : ?>
 
-			<div class="col-12 col-sm-5">
-				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
-			</div>
+				<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+
+				<div class="row" id="customer_details">
+					<div class="col-12">
+						<?php do_action( 'woocommerce_checkout_billing' ); ?>
+					</div>
+
+					<div class="col-12">
+						<?php do_action( 'woocommerce_checkout_shipping' ); ?>
+					</div>
+				</div>
+
+				<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+			
+			<?php endif; ?>
 		</div>
 
-		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+		<div class="col-12 col-sm-6">
 
-	<?php endif; ?>
+			<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'bensemangat' ); ?></h3>
 
-	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'bensemangat' ); ?></h3>
+			<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
-	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+			<div id="order_review" class="woocommerce-checkout-review-order">
+				<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+			</div>
 
-	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+			<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+
+		</div>
 	</div>
-
-	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
 
 </form>
 
